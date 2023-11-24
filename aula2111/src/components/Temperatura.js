6823c4f46f4611f3a64cb9b9284864607bf6f66f
Igor-Gabriel-Santos <Gabrieligor022@gmail.com>
2023-11-24 23:48:35 +0000

    save03

--- aula2111/src/components/Temperatura.js
+++ aula2111/src/components/Temperatura.js
@@ -8,15 +8,15 @@ function Temperatura() {
     const [contador2, setContador2] = useState(0);
     
     const calcularCelsius = () => {
-        contador = (Celsius * 1.8) + 32 
+       const resultado1 = (Celsius * 1.8) + 32
+        setContador(resultado1)
     }
 
     const calcularFahrenheit = () => {
-        contador = (Fahrenheit * 1.8) + 32 
+       const resultado2 = (Fahrenheit - 32) / 1.8
+        setContador2(resultado2)
     }
 
-   
-  
     return (
       <div className='divFoda'>
         <h1>Conversor de Celsius para Fahrenheit e vice e versa</h1>
@@ -29,18 +29,13 @@ function Temperatura() {
         <input type='number' value={Fahrenheit} onChange={(e) => setFahrenheit(e.target.value)}/>
         <br/>
         <br/>
-        <button onClick={calcularFahrenheit} {...() => setContador2(contador2)}>Calcular Fahrenheit para Celcius </button>
-        
-        <p>A quantidade de Celcius para Fahrenheit foi de: {contador}</p>
-        <p>A quantidade de Fahrenheit para Celcius foi de: {contador2}</p>
+        <button onClick={calcularFahrenheit}>Calcular Fahrenheit para Celcius </button>
+        <br/>
+        <br/>
+        {contador !== null && <p>A quantidade de Celcius para Fahrenheit foi de: {contador}</p>}
+        {contador2 !== null && <p>A quantidade de Fahrenheit para Celcius foi de: {contador2}</p>}
         
       </div>
     )
-
   }
-
-
-  
-  
-
 export default Temperatura
